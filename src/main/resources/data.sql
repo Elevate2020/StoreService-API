26d3be51251b10cace9bd75b9151cbe2ac371cc1
swarajbiswal <swaraj.biswal@target.com>
2020-02-19 16:56:58 +0530

    Data change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (1,'900 Nicollet Mall', 'Minneapolis', 'MN','55402');
+  (1,'900 Nicollet Mall', 'MN', 'Minneapolis','55402');
 
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (2,'1329 5th South Street SE', 'Minneapolis', 'MN','55414');
+  (2,'1329 5th South Street SE', 'MN', 'Minneapolis','55414');
 
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (3,'2500 East Lake Street', 'Minneapolis', 'MN','55406');
+  (3,'2500 East Lake Street', 'MN', 'Minneapolis','55406');
 
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (4,'1650 New Brighton Blvd', 'Minneapolis', 'MN','55413');
+  (4,'1650 New Brighton Blvd', 'MN', 'Minneapolis','55413');
 
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (5,'1300 W Lake Street', 'Minneapolis', 'MN','55408');
+  (5,'1300 W Lake Street', 'MN', 'Minneapolis','55408');
 
 INSERT INTO TBL_STORE (id, address,city, name, zip) VALUES
-  (6,'3601 Highway 100 S', 'St Louis Park', 'MN','55416');
+  (6,'3601 Highway 100 S', 'MN', 'Minneapolis','55416');
